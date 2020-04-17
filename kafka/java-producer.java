5a3d834bda9bc8dda8ddb908bd7f76c05904454a
shivam-dhaka <60032159+shivam-dhaka@users.noreply.github.com>
2020-04-17 14:34:41 +0530

    Update java-producer.java

--- kafka/java-producer.java
+++ kafka/java-producer.java
@@ -17,17 +17,7 @@ public class SSLKafkaProducerDemo
     properties.setProperty("bootstrap.servers", kafkaBrokers);
     properties.setProperty("key.serializer", org.apache.kafka.common.serialization.StringSerializer.class.getName());
     properties.setProperty("value.serializer", org.apache.kafka.common.serialization.StringSerializer.class.getName());
-    properties.setProperty("security.protocol", "SASL_SSL"); //On-prem Only
-    properties.setProperty("sasl.kerberos.service.name", "kafka");  //On-prem Only
-    properties.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "C:\\Streaming\\server.truststore.jks");
-    properties.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG,  "Welcome@123");
-//    properties.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "JKS"); //AWS Only
-//    properties.setProperty("security.protocol", "SSL");  //AWS Only
-//    properties.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "C:\\Streaming\\mm-devtest.jks");
-//    properties.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG,  "Welcome@123");
-    properties.setProperty("acks", "1");
-    properties.setProperty("retries", "3");
-    properties.setProperty("linger.ms", "1");
+    properties.setProperty("security.protocol", "SASL_SSL");
 
 
     org.apache.kafka.clients.producer.Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer(properties);
